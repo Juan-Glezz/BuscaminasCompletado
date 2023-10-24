e1441070e67f7ed7ddfd1c95d6f306721bb6c40e
Juan-Glezz <jgonram1210@iesmartinezm.es>
2023-10-24 14:05:53 +0200

    primer commit

---
+++ buscaminas/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path("TableroBuscaminas" , views.form_tablero, name="TableroBuscaminas"),
+]
